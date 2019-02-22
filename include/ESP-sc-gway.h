9fdec2de286b22beb8b3e49c5639758d6fda8dd4
Nate Williams <nate@yogotech.com>
2019-02-21 18:25:46 -0700

    Updates for the TTGO V1 board in PlatformIO and the configuration

--- include/ESP-sc-gway.h
+++ include/ESP-sc-gway.h
@@ -87,7 +87,7 @@
 //	4: ESP32 TTGO pinning (should work for 433 and OLED too).
 //	5: ESP32 TTGO EU433 MHz with OLED
 //	6: Other, define your own in loraModem.h
-#define _PIN_OUT 1
+#define _PIN_OUT 4
 
 // Gather statistics on sensor and Wifi status
 // 0= No statistics
@@ -135,7 +135,7 @@
 // OLED==0; No OLED display connected
 // OLED==1; 0.9 Oled Screen based on SSD1306
 // OLED==2;	1"3 Oled screens for Wemos, 128x64 SH1106
-#define OLED 2
+#define OLED 1
 
 
 // Define whether we want to manage the gateway over UDP (next to management 
